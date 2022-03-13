30e144f46090c690d2d80d368bd2a091a9e40ce0
CheginArtyom <99319275+CheginArtyom@users.noreply.github.com>
2022-03-13 23:10:20 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,25 @@
-// Copyright 2021 NNTU-CS
+// Copyright 2021 CheginArtyom
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+  int h = size - 1;
+  int l = 0;
+  int mid;
+  while (l <= h) {
+    mid = (l + h) / 2;
+    if (value == (arr + mid)) {
+      l = h = mid;
+      while ((arr + h) == (arr + h + 1)) {
+        h++;
+      }
+      while ((arr + l) == *(arr + l - 1)) {
+        l--;
+      }
+      return h - l + 1;
+    } else if (value > *(arr + mid)) {
+      l = mid + 1;
+    } else if (value < *(arr + mid)) {
+      h = mid - 1;
+    }
+  }
+  return 0;
 }
